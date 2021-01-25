a633341603d8386472b265668e0b1bdb95f64b39
hyhgo0903 <hyhgo0903@gmail.com>
2021-01-25 09:17:07 +0900

    테스트겸 경로수정

--- loadingScene+yoongho.cpp
+++ loadingScene+yoongho.cpp
@@ -4,7 +4,7 @@
 void loadingScene::yoonghoInit()
 { // 로디드가 뭐냐면 로딩바 올리기용 함수. 이미지 추가할떄마다 넣어주길	
 
-	SOUNDMANAGER->addSound("사운드추가예시", "PTeSto00.wav", false, false);
+	SOUNDMANAGER->addSound("사운드추가예시", "sound/PTeSto00.wav", false, false);
 	soundLoaded();
 
 	IMAGEMANAGER->addFrameImage("tile", "isoprac.bmp", 448, 32, 7, 1, true, RGB(255, 0, 255));
