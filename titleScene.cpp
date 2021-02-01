7033649737e1ae5f66b0cefea2e0b86d0c87ab1a
a4523_id <a4523bcdefg@gmail.com>
2021-02-01 12:44:37 +0900

    타이틀 클릭하면 이돌하도록 만들었습니다.

--- titleScene.cpp
+++ titleScene.cpp
@@ -13,6 +13,11 @@ HRESULT titleScene::init()
 {
 	CAMERAMANAGER->setCameraX(0);
 	CAMERAMANAGER->setCameraY(0);
+	_image = IMAGEMANAGER->addImage("타이틀메뉴", "image/title.bmp", WINSIZEX, WINSIZEY, true, RGB(255, 0, 255));
+	_gameStart = RectMakeCenter(WINSIZEX / 2, 350, 200, 60);
+	_mapTool = RectMakeCenter(WINSIZEX / 2, 430, 200, 60);
+	_option = RectMakeCenter(WINSIZEX / 2, 510, 200, 60);
+	_exit = RectMakeCenter(WINSIZEX / 2, 592, 200, 60);
 	return S_OK;
 }
 
@@ -20,21 +25,38 @@ void titleScene::release()
 {
 }
 
+
 void titleScene::update()
 {
-	if (KEYMANAGER->isStayKeyDown('A'))
+	if (KEYMANAGER->isOnceKeyDown(MK_LBUTTON))
 	{
-		SCENEMANAGER->changeScene("메인씬");
+		if (PtInRect(&_gameStart, _ptMouse) == true && PtInRect(&_gameStart, _ptMouse) == true)
+		{
+			SCENEMANAGER->changeScene("메인씬");
+		}
+
+		else if (PtInRect(&_mapTool, _ptMouse) == true && PtInRect(&_mapTool, _ptMouse) == true)
+		{
+			SCENEMANAGER->changeScene("맵툴");
+		}
+
+		if (KEYMANAGER->isOnceKeyDown(MK_LBUTTON) && PtInRect(&_option, _ptMouse) == true)
+		{
+			// 볼륨조절 키입력 할 예정입니다.
+			SCENEMANAGER->changeScene("옵션");
+		}
+		if (KEYMANAGER->isOnceKeyDown(MK_LBUTTON) && PtInRect(&_exit, _ptMouse) == true)
+		{
+			// 나가는 함수 찾아서 적을예정
+		}
 	}
-	if (KEYMANAGER->isStayKeyDown('S'))
-	{
-		SCENEMANAGER->changeScene("맵툴");
-	}
+
 }
 
 void titleScene::render()
 {
-	TextOut(getMemDC(), WINSIZEX / 3, WINSIZEY / 2,
-		"A스타트     S맵툴     D옵션(아직X)",
-		strlen("A스타트     S맵툴     D옵션(아직X)"));
+
+	_image->render(getMemDC());
+
+
 }
--- titleScene.h
+++ titleScene.h
@@ -4,6 +4,13 @@ class titleScene : public gameNode
 {
 private:
 
+	image* _image;
+	RECT _gameStart;
+	RECT _mapTool;
+	RECT _option;
+	RECT _exit;
+	POINT _point;
+
 public:
 	titleScene();
 	~titleScene();
@@ -12,5 +19,10 @@ public:
 	virtual void release();	//메모리 해제 함수
 	virtual void update();	//연산 전용
 	virtual void render();	//그리기 전용
+
+	RECT getGameStart() { return _gameStart; }
+	RECT getMaptool() { return _mapTool; }
+	RECT getOption() { return _option; }
+	RECT getExit() { return _exit; }
 };
 
